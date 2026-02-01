ee0bf44588dc49b4ad0c97b2f30fb7f7ce0325de
khamzaevasad <xamzayevasad442@gmail.com>
2026-02-02 03:04:50 +0900

    feat: implement shared and auth layouts, signup page, and form validation schema

---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { signUpSchema } from "@/app/schemas/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
+export default function SignupPage() {
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign up</CardTitle>
+        <CardDescription>Create an account to get started</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input placeholder="John Doe" {...field} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Email</FieldLabel>
+                  <Input placeholder="John@doe.com" type="email" {...field} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+
+                  <Controller
+                    name="password"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field>
+                        <FieldLabel>Password</FieldLabel>
+                        <Input placeholder="*****" type="password" {...field} />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                  <Button>Sign Up</Button>
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
